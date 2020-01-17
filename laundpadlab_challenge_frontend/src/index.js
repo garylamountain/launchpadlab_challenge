8a8cb10a0b6b771b5274c3d58d9c434235f866ff
github username <github email address>
2020-01-17 11:15:07 -0600

    created a functioning fetch call to a single repo

--- laundpadlab_challenge_frontend/src/index.js
+++ laundpadlab_challenge_frontend/src/index.js
@@ -1,6 +1,12 @@
 function main(){
     let feed = document.querySelector('main');
     feed.innerHTML = 'HELLO WORLD';
+    fetch("https://api.github.com/repos/facebook/react")
+    // fetch("https://api.github.com/repos/facebook/react/stats/commit_activity")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
 }
 
 main()
